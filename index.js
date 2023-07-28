3353c70644c52222e72cd6fb77ef064a74256448
Adeshina Adenuga <Nuga0909@gmail.com>
2023-07-28 06:13:05 +0100

    create files and populate cart

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("maggy");
+
+const myCart = [
+    {
+        title: "2023 Men Casual Sneakers",
+        supplier: "Puma",
+        available: "few units left",
+        Amount: 6000,
+        isLike: true
+    },
+    {
+        title: "XIAOMI REdmi 10",
+        supplier: "Xiaomi store",
+        available: "7 units left",
+        Amount: 72000,
+        isLike: true
+    },
+    {
+        title: "Oraimo 10000 mAh power bank",
+        supplier: "Oraimo store",
+        available: "2 units left",
+        Amount: 12000,
+        isLike: true
+    },
+    {
+        title: "Edifier Wireless Bluetooth Headphone",
+        supplier: "Sammy inc.",
+        available: "few units left",
+        Amount: 25000,
+        isLike: true
+    }
+]
